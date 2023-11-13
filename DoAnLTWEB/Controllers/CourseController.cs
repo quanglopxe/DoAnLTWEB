4503c3e6f1cde7e75e280d06b018890594efd07d
quanglopxe <quanglopxe@gmail.com>
2023-11-13 20:40:39 +0700

    push HomeController

---
+++ DoAnLTWEB/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAnLTWEB.Models;
+
+namespace DoAnLTWEB.Controllers
+{
+    public class CourseController : Controller
+    {
+        QuanLyKhoaHocDataContext db = new QuanLyKhoaHocDataContext();
+        //
+        // GET: /Course/
+        public ActionResult Course()
+        {
+            List<KhoaHoc> listCourse = new List<KhoaHoc>();
+            listCourse = db.KhoaHocs.ToList();
+            return View(listCourse);
+        }
+	}
+}
